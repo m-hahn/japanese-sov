611ead25eb0972e44cae09a86e4561e76877bb70
Michael Hahn <mhahn29@gmail.com>
2022-03-24 21:15:58 -0700

    code

---
+++ analyze-depl-diff_korean.R
@@ -0,0 +1,85 @@
+library(tidyr)
+library(dplyr)
+library(ggplot2)
+
+data = read.csv("results/korean/depl-difference.tsv", sep="\t", row.names=NULL)
+
+data2 = data %>% group_by(Dependency) %>% summarise(Number = NROW(Difference), Reduction = sum(Difference < 0) / sum(Difference!=0), Mean = mean(Difference), SD = sd(Difference)) %>% mutate(t=Mean/(SD / sqrt(Number)))
+
+
+data2[order(-data2$Number),] %>% print(n=40)
+
+
+
+
+data2=data %>% filter(Dependency == "lifted_mark")
+plot = ggplot(data=data2, aes(x=Difference)) + geom_bar()
+plot = plot + xlim(quantile(data2$Difference, 0.01)-1, quantile(data2$Difference, 0.99)+1)
+
+
+
+data2=data %>% filter(Dependency == "nsubj")
+plot = ggplot(data=data2, aes(x=Difference)) + geom_bar()
+plot = plot + xlim(quantile(data2$Difference, 0.01)-1, quantile(data2$Difference, 0.99)+1)
+
+
+data2=data %>% filter(Dependency == "obl")
+plot = ggplot(data=data2, aes(x=Difference)) + geom_bar()
+plot = plot + xlim(quantile(data2$Difference, 0.01)-1, quantile(data2$Difference, 0.99)+1)
+
+
+
+
+
+
+data = read.csv("results/korean/depl-perSent-Korean_2.4.tsv", sep="\t", row.names=NULL)
+data$Diff = data$Real - data$Counterfactual
+summary(lm(Diff ~ 1, data=data))
+
+
+data2 = data %>% filter(Diff != 0)
+binom.test(sum(data2$Diff < 0), n=nrow(data2), p=0.5, alternative="g")
+
+
+
+
+data = read.csv("results/korean/depl-perSent-Korean-GSD_2.4.tsv", sep="\t", row.names=NULL)
+data$Diff = data$Real - data$Counterfactual
+summary(lm(Diff ~ 1, data=data))
+
+binom.test(sum(data$Diff < 0), n=nrow(data), p=0.5, alternative="t")
+
+data2 = data %>% filter(Diff != 0)
+binom.test(sum(data2$Diff < 0), n=nrow(data2), p=0.5, alternative="g")
+
+
+GSD = data %>% mutate(Corpus = "GSD")
+
+
+
+
+data = read.csv("results/korean/depl-perSent-Korean-Kaist_2.4.tsv", sep="\t", row.names=NULL)
+data$Diff = data$Real - data$Counterfactual
+summary(lm(Diff ~ 1, data=data))
+
+data2 = data %>% filter(Diff != 0)
+binom.test(sum(data2$Diff < 0), n=nrow(data2), p=0.5, alternative="g")
+
+
+Kaist = data %>% mutate(Corpus = "Kaist")
+
+
+
+
+plot = ggplot(data=rbind(GSD, Kaist) %>% filter(Diff != 0), aes(x=Corpus, y=Diff)) + geom_violin()
+plot = plot + ylim(-20, 20)
+
+
+plot = ggplot(data=rbind(GSD, Kaist) %>% filter(Diff != 0), aes(x=Diff)) + geom_bar() + facet_wrap(~Corpus)
+plot = plot + xlim(-40, 40) + xlab("Dependency Length Reduction in Counterfactual Order") + ylab("Sentences")
+ggsave(plot, file="figures/dependency_lentgh_differences_byCorpus_korean.pdf", width=6, height=3)
+
+
+
+
+
